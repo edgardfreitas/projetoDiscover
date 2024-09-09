c7a725e81cd898a2a0e4ad893546ccab6faa5e51
Edgard <edgardfp@gmail.com>
2024-09-09 10:09:34 -0300

    HTML Semantic Elements and var (--footer-color)

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function toggleMode() {
   html.classList.toggle("light")
 
   // pegar a tag img
-  const img = document.querySelector("#profile img")
+  const img = document.getElementById("profile__image")
 
   // substituir a imagem
   if (html.classList.contains("light")) {
